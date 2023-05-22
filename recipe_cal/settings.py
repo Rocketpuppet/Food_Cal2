cfb4d23de1f6cf7b7803efd190428503efe52075
Rocketpuppet <67485112+Rocketpuppet@users.noreply.github.com>
2023-05-22 16:43:50 -0400

    Update settings.py

--- recipe_cal/settings.py
+++ recipe_cal/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = ['foodcal2-production.up.railway.app','www.foodpricecalcualtor.store','127.0.0.1']
+ALLOWED_HOSTS = ['foodcal2-production.up.railway.app','https://www.foodpricecalcualtor.store','127.0.0.1']
 
 
 # Application definition
